585954770ccef659fc39ecdb5579774ba7eedd5b
Huper12345 <huper12345@gmail.com>
2023-04-23 17:27:50 +0300

    add header component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,10 @@
 import "./Home.scss";
+import { Header } from "../../components/Header";
 
 export const Home = () => {
     return (
         <div className="Home">
-            Привет реакт
+            <Header />
         </div>
     )
 };
