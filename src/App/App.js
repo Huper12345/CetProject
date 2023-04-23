f33e8ce4421eed071860fd18f5df5da885d59c7a
Huper12345 <huper12345@gmail.com>
2023-04-23 17:19:56 +0300

    add react directory

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import React from "react"
+import { Home } from '../pages/Home';
+
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+};
+
