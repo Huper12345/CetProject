b5dc3a52abb7c00eb6d8611e2d13b9373141be71
Huper12345 <huper12345@gmail.com>
2023-04-27 22:01:40 +0300

    update blocks intro, lekitem

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,5 +1,6 @@
 import './Intro.scss';
 import { LekItem } from './LekItem';
+import { Preview } from './Preview';
 import TabPack from "../../helpers/Images/Packs/Tab3.png"
 import TabPack2 from "../../helpers/Images/Packs/kaplip5.png"
 import ChildMarker from "../../helpers/Images/Markers/son2.png"
@@ -12,22 +13,25 @@ export const Intro = () => {
         <div className="Intro">
             <div className='container'>
                 <div className="Intro-inner">
-                    <LekItem 
-                    Img={TabPack}
-                    marker1={ChildMarker}
-                    LekText1="С 6 лет"
-                    marker2={pills}
-                    LekText2="Таблетки"
-                    LekText3="Лекарственный препарат цетиризин в таблетках это эффективное средство против аллергии для взрослых"
-                    />
-                    <LekItem 
-                    Img={TabPack2}
-                    marker1={babymarker}
-                    LekText1="С 6 месяцев"
-                    marker2={dropMarker}
-                    LekText2="Капли"
-                    LekText3="Лекарственный препарат цетиризин в каплях это эффективное средство против аллергии для детей"
-                    />
+                    <Preview />
+                    <div className='Intro-select'>
+                        <LekItem 
+                        Img={TabPack}
+                        marker1={ChildMarker}
+                        LekText1="С 6 лет"
+                        marker2={pills}
+                        LekText2="Таблетки"
+                        LekText3="Лекарственный препарат цетиризин в таблетках это эффективное средство против аллергии для взрослых"
+                        />
+                        <LekItem 
+                        Img={TabPack2}
+                        marker1={babymarker}
+                        LekText1="С 6 месяцев"
+                        marker2={dropMarker}
+                        LekText2="Капли"
+                        LekText3="Лекарственный препарат цетиризин в каплях это эффективное средство против аллергии для детей"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
