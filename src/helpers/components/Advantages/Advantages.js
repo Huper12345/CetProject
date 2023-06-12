82079a2ecfb9e37417ce3af05189edb38874526b
Huper12345 <huper12345@gmail.com>
2023-06-12 22:43:29 +0300

    udpate dosage usage, advantages, text

--- src/helpers/components/Advantages/Advantages.js
+++ src/helpers/components/Advantages/Advantages.js
@@ -4,7 +4,20 @@ import "./Advantages.scss";
 import { PlusIcon } from "./PlusIcon";
 
 
-export const Advantages = ({buttonText, droptext, subtext, Style="Advantages-Button"}) => {
+export const Advantages = ({
+  buttonText, 
+  droptext, 
+  subtext1,
+  subtext2, 
+  subtext3, 
+  subtext4, 
+  subtext5, 
+  addtext1,
+  addtext2,
+  addtext3,
+  addtext4,
+  addtext5,
+  Style="Advantages-Button"}) => {
     const [open, setOpen] = useState(false);
   
     const handleOpen = () => {
@@ -22,7 +35,13 @@ export const Advantages = ({buttonText, droptext, subtext, Style="Advantages-But
         {open ? (
           <ul className="Dropdown-Menu">
             <li className="Drop-Menu-Item">
-              <h3 className="Drop-Menu-Text">{droptext}<p className="Advantages-Menu-Subtext">{subtext}</p></h3>
+              <h3 className="Drop-Menu-Text">{droptext}
+              <p className="Advantages-Menu-Subtext"><i className="Advantages-Menu-AdditionalText">{addtext1}</i>{subtext1}</p>
+              <p className="Advantages-Menu-Subtext"><i className="Advantages-Menu-AdditionalText">{addtext2}</i>{subtext2}</p>
+              <p className="Advantages-Menu-Subtext"><i className="Advantages-Menu-AdditionalText">{addtext3}</i>{subtext3}</p>
+              <p className="Advantages-Menu-Subtext"><i className="Advantages-Menu-AdditionalText">{addtext4}</i>{subtext4}</p>
+              <p className="Advantages-Menu-Subtext"><i className="Advantages-Menu-AdditionalText">{addtext5}</i>{subtext5}</p>
+              </h3>
             </li>
           </ul>
         ) : null}
